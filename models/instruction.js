fde9adf4e43925e61f0bd25ee63606a13b57d8e3
war-turtle <kartikyadav71199@gmail.com>
2018-09-14 05:29:38 -0700

    instruction table made

---
+++ models/instruction.js
@@ -0,0 +1,23 @@
+'use strict';
+let connection = require('./connection');
+let User = require('./user');
+let Products = require('./products');
+
+let Instruction = connection.sequelize.define('instructions', {
+    id: {
+        type: connection.Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    description: {
+        type: connection.Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+Instruction.belongsTo(User);
+Instruction.belongsTo(Products, {foreignKey: 'productId', targetKey: 'productId'});
+Instruction.sync({force: false});
+
+module.exports = Instruction;
--- models/models.js
+++ models/models.js
@@ -2,12 +2,14 @@ var user = require('./user');
 var response = require('./response');
 var products = require('./products');
 var recipie = require('./recipie');
+var instruction = require('./instruction');
 
 var models = {
     user : user,
     response : response,
     products: products,
-    recipie: recipie
+    recipie: recipie,
+    instruction: instruction
 };
 
 module.exports = models;
